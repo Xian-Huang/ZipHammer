0344bdbd04929cde860e8d084c2c9b45202390a1
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-06-13 11:48:37 +0800

    参数path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs::File};
+use clap::{command, Parser};
+
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// 文件路径
+    #[arg(short, long)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args_matcher = Args::parse();
+    dbg!(args_matcher);
 }
