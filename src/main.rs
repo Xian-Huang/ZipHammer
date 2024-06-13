b773aa1568ea419677d6d9350bfd71651455736c
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-06-13 12:09:47 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ struct Args {
     /// 文件路径
     #[arg(short, long)]
     path: String,
+ 
+    /// 是否暴力破解
+    #[arg(short,long,default_value_t=false)]
+    e:bool
 }
 
-fn main() {
+fn main(){
     let args_matcher = Args::parse();
-    dbg!(args_matcher);
 }
