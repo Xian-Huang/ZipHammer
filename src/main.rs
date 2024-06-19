80fad71ca232ebe8b07a9a50e72f95902aa1e85f
LiDingyi <148949408+LiDingyiii@users.noreply.github.com>
2024-06-19 11:12:09 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -37,16 +37,16 @@ impl WordType {
         wordtypes_res
     }
 
-    fn create_until(self:&Self){
+    fn create_until(self:&Self)->u8{
         // 创建密码元素
         match self {
-            WordType::Number => todo!(),
+            WordType::Number => self.create_number(),
             WordType::Letter => todo!(),
             WordType::Special => todo!(),
         }
     }
 
-    fn create_number()->u8{
+    fn create_number(self:&Self)->u8{
         rand::thread_rng().gen_range(0..=9)
     }
 
@@ -65,14 +65,18 @@ struct PasswordCreater {
 }
 
 impl PasswordCreater {
-    fn new(length: u8,wordtypes:Vec<&WordType>) {
+    fn new(length: u8,wordtypes:Vec<&WordType>) -> Self{
         let wt = WordType::create_wordtypes(length, wordtypes);
-
+        let password:Vec<u8> = Vec::new();
+        let mut password_wt = Vec::new();
         for i in 0..length {
             todo!("创建指定长度的密码");
             let select_wt = *wt.get(i as usize).unwrap();
             let util = select_wt.create_until();
+            password_wt.push(*select_wt);
         }
+
+        PasswordCreater { password: password, types: password_wt }
     }
 
     fn create_password_set() {}
